bbe6bdf1e374dc0f921e80e1db08e43360d6ff23
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-04-23 16:54:20 +0530

    Add files via upload

--- Projects/CareerMitra/CareerMitra-App/src/main/java/com/example/careermitra/IntroScreen.java
+++ Projects/CareerMitra/CareerMitra-App/src/main/java/com/example/careermitra/IntroScreen.java
@@ -2,29 +2,37 @@ package com.example.careermitra;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class IntroScreen extends AppCompatActivity {
-
-    Button goToMainScreen;
+    AppCompatButton goToMainScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_intro_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
 
         goToMainScreen = findViewById(R.id.goToMainScreen);
+
         goToMainScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(IntroScreen.this, MainActivity.class));
-                finish();
+                startActivity(new Intent(IntroScreen.this,MainActivity.class));
             }
         });
-
     }
 }
